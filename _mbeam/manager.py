56413b6f0c63279d8f337e8338fffd98a48be10f
haehn <haehn@seas.harvard.edu>
2015-05-04 21:10:00 -0400

    Try something else.

--- _mbeam/manager.py
+++ _mbeam/manager.py
@@ -73,10 +73,15 @@ class Manager(object):
     if len(mfov_glob) > 0:
       return 'FOV'
 
+    first_sub_dirs = next(os.walk(data_path))[1]
+    if len(first_sub_dirs) > 0:
+
+      section_glob = glob.glob(os.path.join(data_path, first_sub_dirs[0], Constants.IMAGE_COORDINATES_FILE))
+
     # section_glob = glob.glob(data_path + os.sep + '*' + os.sep + Constants.IMAGE_COORDINATES_FILE)
 
-    # if len(section_glob) > 0:
-    #   return 'SECTION'
+      if len(section_glob) > 0:
+        return 'SECTION'
 
     # scan_glob = glob.glob(data_path + os.sep + '*' + os.sep + '*' + os.sep + Constants.IMAGE_COORDINATES_FILE)    
 
