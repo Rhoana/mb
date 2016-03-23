bf49139a591c3cda62bc8e6cfd8f2329bd0bff95
Justin Riley <justin.t.riley@gmail.com>
2016-03-22 21:54:30 -0400

    cli: add --address option for webserver

--- mbeam/cli.py
+++ mbeam/cli.py
@@ -7,6 +7,8 @@ from mbeam import webserver
 def get_parser():
     parser = argparse.ArgumentParser(description='mbeam server')
     parser.add_argument('-p','--port', help='Port to listen on', default=2001)
+    parser.add_argument('-a','--address', help='Address to listen on',
+                        default='127.0.0.1')
     return parser
 
 
@@ -17,5 +19,5 @@ def main():
     mgr = manager.Manager()
     mgr.start()
 
-    websrv = webserver.WebServer(mgr, args.port)
+    websrv = webserver.WebServer(mgr, port=args.port, address=args.address)
     websrv.start()
